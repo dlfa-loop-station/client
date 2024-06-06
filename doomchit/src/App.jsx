72f1a78cf46b6e5bb296e45c7fa363cb70da1ec3
KwonET <dlxl3652zz@gmail.com>
2024-06-06 21:24:42 +0900

    [init] setting

---
+++ doomchit/src/App.jsx
@@ -0,0 +1,14 @@
+import AudioRecord from "./components/AudioRecord";
+
+export default function App() {
+  return (
+    <>
+      <div className="App">
+        <header className="App-header">
+          <AudioRecord />
+          Doomchit
+        </header>
+      </div>
+    </>
+  );
+}
